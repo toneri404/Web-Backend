56bf2f77b6c4710c1a67b3a33867391a5a8866d7
Toneri <104565824+toneri404@users.noreply.github.com>
2025-11-02 22:31:01 +0600

    Add files via upload

---
+++ controllers/order.controller.js
@@ -0,0 +1,22 @@
+const Order = require('../models/order');
+
+// Create a new order
+exports.createOrder = async (req, res) => {
+  try {
+    const order = new Order(req.body);
+    await order.save();
+    res.status(201).json(order);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Get all orders
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().populate('user').populate('products.product');
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
